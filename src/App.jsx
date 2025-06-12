61506961088b4ee364ed1f30b041b345bb3435f7
imranshaik2718 <imranshaik1145@gmail.com>
2025-06-12 12:43:12 +0530

    NavBar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className='bg-gray-200'>
+     <Navbar/>
+     
+     
+     </div>
+   
+  )
+}
+
+export default App
